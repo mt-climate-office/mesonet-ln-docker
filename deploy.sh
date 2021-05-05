b1fa0d83bb5c0a97425f557bf60e8e20c0e71aad
Kyle Bocinsky <bocinsky@gmail.com>
2021-05-04 22:15:15 -0600

    docker-compose

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 git pull
 eval "$(ssh-agent -s)"
-ssh-add /home/kbocinsky/.ssh/id_ed25519
-export COMPOSE_OPTIONS="-e SSH_AUTH_SOCK"
+#ssh-add /home/kbocinsky/.ssh/id_ed25519
 export DOCKER_BUILDKIT=1 # or configure in daemon.json
 export COMPOSE_DOCKER_CLI_BUILD=1
 docker-compose up -d --build
