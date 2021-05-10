881a4d49f718fdf9b3630754e7125df6f3a5a0c6
Kyle Bocinsky <bocinsky@gmail.com>
2021-05-10 09:56:15 -0600

    multipath fix

--- loggernet/backup_config.sh
+++ loggernet/backup_config.sh
@@ -4,7 +4,13 @@ cd /opt/mesonet-ln-config
 
 git pull
 
-cora_cmd < /opt/backup_config.cora
+#cora_cmd < /opt/backup_config.cora
+cora_cmd \
+--echo=off \
+--input='{
+connect localhost;
+create-backup-script /opt/mesonet-ln-config/network_config.cora;
+}'
 
 git add .
 git commit -m "created network backup"
