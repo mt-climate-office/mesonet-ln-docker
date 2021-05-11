e0d6539f20e28832c842dfc06f4ee23f226a8b7a
Kyle Bocinsky <bocinsky@gmail.com>
2021-05-11 12:17:34 -0600

    testing bash posting

--- loggernet/report_stations.sh
+++ loggernet/report_stations.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-cora_cmd \
---echo=off \
---input='{
-connect localhost; 
-list-devices;
-}'
+trap 'rm -f "$TMPFILE"' EXIT
 
+TMPFILE=$(mktemp) || exit 1
+echo "Our temp file is $TMPFILE"
+
+## Get a station's data
+cora_cmd \
+--echo=on \
+--input="{
+connect localhost; 
+logger-query-ex $1 FiveMin $TMPFILE most-recent 1 --format=\"CSIXML\";
+}"
+
+cat $TMPFILE |\
+xq . 
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data . \
+#  https://mesonet.climate.umt.edu/api/loggernet
