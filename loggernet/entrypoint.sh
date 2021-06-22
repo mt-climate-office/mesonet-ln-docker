96c237d71989b0eb4fa7264142aeb4c3bb8603c9
Kyle Bocinsky <bocinsky@gmail.com>
2021-06-22 15:59:39 -0600

    trying to remove empty loggernet

--- loggernet/entrypoint.sh
+++ loggernet/entrypoint.sh
@@ -11,14 +11,7 @@ echo "${SSH_KEY}" > ~/.ssh/id_rsa
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
 
-env >> ~/env.log
-echo 'PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' > /etc/crontab
-echo 'SHELL=/bin/bash' >> /etc/crontab
-echo '* * * * * root bash -c "source $HOME/env.log; source /opt/backup_config.sh"' >> /etc/crontab
-echo '*/5 * * * * root bash -c "source $HOME/env.log; source /opt/update_stations.sh"' >> /etc/crontab
-echo '0 * * * * root bash -c "source $HOME/env.log; source /opt/update_station_programs.sh"' >> /etc/crontab
 
-echo "Host *\n    StrictHostKeyChecking no\n    UserKnownHostsFile=/dev/null\n" > ~/.ssh/config
 
 # Configure our user and email to commit as.
 echo "Configuring git"
@@ -32,6 +25,15 @@ echo "Starting LoggerNet Service"
 cd /opt/mesonet-ln-config
 git pull
 bash /opt/restore_config.sh
+
+env >> ~/env.log
+echo 'PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' > /etc/crontab
+echo 'SHELL=/bin/bash' >> /etc/crontab
+#echo '* * * * * root bash -c "source $HOME/env.log; source /opt/backup_config.sh"' >> /etc/crontab
+echo '*/5 * * * * root bash -c "source $HOME/env.log; source /opt/update_stations.sh"' >> /etc/crontab
+echo '0 * * * * root bash -c "source $HOME/env.log; source /opt/update_station_programs.sh"' >> /etc/crontab
+
+echo "Host *\n    StrictHostKeyChecking no\n    UserKnownHostsFile=/dev/null\n" > ~/.ssh/config
 
 # start cron
 service cron start
