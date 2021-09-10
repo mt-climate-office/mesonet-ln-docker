154b2221e72c3b013fbfaf5e55acc8e01a0c72b5
Kyle Bocinsky <bocinsky@gmail.com>
2021-09-10 13:37:12 -0600

    change device naming convention

--- loggernet/update_station_programs.sh
+++ loggernet/update_station_programs.sh
@@ -12,7 +12,7 @@ do
         --echo=on \
         --input="{
             connect localhost;
-            get-program-file $station --use-cache=true --file-name=$station.txt --file-path=/opt/mesonet-ln-config/;
+            get-program-file ${station}Photo --use-cache=true --file-name=$station.txt --file-path=/opt/mesonet-ln-config/;
             }"
     
     [ -s \\$station.txt ] && mv \\$station.txt $station.txt || rm -f \\$station.txt
