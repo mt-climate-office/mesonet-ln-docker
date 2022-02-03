cf0e4b0052bf76e38b2c842d9520e600f431aa27
Kyle Bocinsky <bocinsky@gmail.com>
2022-02-03 10:29:38 -0700

    too many processes

--- loggernet/update_station.sh
+++ loggernet/update_station.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /opt/report_station.sh ${1}
-/opt/update_station_program.sh ${1}
+#/opt/update_station_program.sh ${1}
--- loggernet/update_stations.sh
+++ loggernet/update_stations.sh
@@ -4,7 +4,11 @@ stations="`curl https://mesonet.climate.umt.edu/api/v2/loggernet/ | jq -r '. | @
 
 echo $stations
 
+n = 4
+
+
 for station in $stations
 do
+    ((i=i%N)); ((i++==0)) && wait
     timeout 5m /opt/update_station.sh $station &
 done
