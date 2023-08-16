1a0b2007499c98939457619b25fa2b3964614d20
colinbrust <colin.brust@gmail.com>
2023-08-16 15:12:40 -0600

    Add station name to POST header.

--- loggernet/report_station.sh
+++ loggernet/report_station.sh
@@ -19,5 +19,5 @@ cat $TMPFILE |
 
 #cat $TMPFILE
 
-curl -X POST -d @$TMPFILE --header "loggernet: push" https://mesonet.climate.umt.edu/api/v2/loggernet/
-curl -X POST -d @$TMPFILE --header "loggernet: push" https://fcfc-mesonet-staging.cfc.umt.edu/api/v2/loggernet/
+curl -X POST -d @$TMPFILE --header "loggernet: push" --header "station: ${1}" https://mesonet.climate.umt.edu/api/v2/loggernet/
+curl -X POST -d @$TMPFILE --header "loggernet: push" --header "station: ${1}" https://fcfc-mesonet-staging.cfc.umt.edu/api/v2/loggernet/
