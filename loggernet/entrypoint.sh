9f59e3c713c15a58e18721a805acdba1f673dade
colinbrust <colin.brust@gmail.com>
2024-08-06 11:47:02 -0600

    pull if exists. Clone otherwise.

--- loggernet/entrypoint.sh
+++ loggernet/entrypoint.sh
@@ -2,10 +2,28 @@
 set -e -x -v
 
 echo "Downloading ln software"
-# Clone private repositories
-git -C /opt/mesonet-ln-software pull || GIT_SSH_COMMAND='ssh -i /run/secrets/SSH_KEY' git clone git@github.com:mt-climate-office/mesonet-ln-software /opt/mesonet-ln-software
-git -C /opt/mesonet-ln-server pull || GIT_SSH_COMMAND='ssh -i /run/secrets/SSH_KEY' git clone git@github.com:mt-climate-office/mesonet-ln-server /opt/mesonet-ln-server
-git -C /opt/mesonet-ln-programs pull || GIT_SSH_COMMAND='ssh -i /run/secrets/SSH_KEY' git clone git@github.com:mt-climate-office/mesonet-ln-programs /opt/mesonet-ln-programs
+# Define the SSH command
+GIT_SSH_COMMAND='ssh -i /run/secrets/SSH_KEY'
+
+# Function to clone or pull a repository
+clone_or_pull() {
+    local repo_url=$1
+    local clone_dir=$2
+
+    if [ -d "$clone_dir" ]; then
+        echo "Directory $clone_dir exists. Performing git pull."
+        $GIT_SSH_COMMAND git -C "$clone_dir" pull
+    else
+        echo "Directory $clone_dir does not exist. Performing git clone."
+        $GIT_SSH_COMMAND git clone "$repo_url" "$clone_dir"
+    fi
+}
+
+# Repositories and their target directories
+clone_or_pull "git@github.com:mt-climate-office/mesonet-ln-software" "/opt/mesonet-ln-software"
+clone_or_pull "git@github.com:mt-climate-office/mesonet-ln-server" "/opt/mesonet-ln-server"
+clone_or_pull "git@github.com:mt-climate-office/mesonet-ln-programs" "/opt/mesonet-ln-programs"
+
 
 printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
 
